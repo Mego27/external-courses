da6f65b98287232a5dee772d7ea604b507013940
Andrew <bestkav27@icloud.com>
2020-03-01 10:08:55 +0300

    repeat send commit after collab

--- src/ex2_js-basics-part2/task-03.js
+++ src/ex2_js-basics-part2/task-03.js
@@ -1,6 +1,6 @@
 function countOddEvenZeroArray(array){
     let arrayCounters = new Array(3).fill(0);// четные / нечетные / нули
-    for (let i = 0;i < array.length; i++){
+    for (let i = 0; i < array.length; i++){
         if (typeof array[i] === 'number' && !isNaN(array[i])){
             if (array[i]%2===1){
                  arrayCounters[1]++;
