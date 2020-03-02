2fdb08e7790f0e0e961a9b73dccfae10cf0ff894
Andrew <bestkav27@icloud.com>
2020-03-02 17:09:48 +0300

    create and add task-06.js

---
+++ src/ex3_js-objects-part1/task-06.js
@@ -0,0 +1,20 @@
+function deepCopyObject(object) { 
+    let newObject;  
+    if (Array.isArray(object)){
+        newObject = [];
+        for (let i = 0; i < object.length; i++){
+            newObject[i] = deepCopyObject(object[i]);
+        }
+    }
+    else if (object instanceof Object){
+        newObject = {};
+        for (let key in object){
+            newObject[key] = deepCopyObject(object[key]);
+        }
+    }
+    else{
+        newObject = object;
+    }
+    return newObject;
+}
+module.exports = deepCopyObject;
