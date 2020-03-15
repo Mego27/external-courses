27f0d8463e766cfd711e3307022caa03e5f837b5
Andrew <bestkav27@icloud.com>
2020-03-15 20:35:26 +0300

    fixed hw04: default assignment added to task-07

--- src/ex4_js-objects-part2/task-07.js
+++ src/ex4_js-objects-part2/task-07.js
@@ -1,5 +1,5 @@
 function cutSpecificString(string, countSymbols){ 
-    let newString;
+    let newString = string;
     if (string.length > countSymbols){
         newString = string.slice(0, countSymbols-1) + "\u2026";
     }
