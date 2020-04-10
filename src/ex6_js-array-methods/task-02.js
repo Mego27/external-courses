19767d9ee7b17633b592a3e4d3946d12a02d9174
Andrew <bestkav27@icloud.com>
2020-04-10 12:42:01 +0300

    fix hw06: delete unnecessary operations

--- src/ex6_js-array-methods/task-02.js
+++ src/ex6_js-array-methods/task-02.js
@@ -1,6 +1,5 @@
 function hasSome(array, callbackFunction){
     for (let i = 0; i < array.length; i++){
-        let item = array[i];
         if (callbackFunction(item, i, array)){
             return true;
         }
--- src/ex6_js-array-methods/task-03.js
+++ src/ex6_js-array-methods/task-03.js
@@ -1,6 +1,5 @@
 function hasEvery(array, callbackFunction){
     for (let i = 0; i < array.length; i++){
-        let item = array[i];
         if (!(callbackFunction(item, i, array))){
             return false;
         }
