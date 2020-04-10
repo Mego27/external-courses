86ee197965d4d7b2df9574eb61d9b8d0ca731e36
Andrew <bestkav27@icloud.com>
2020-04-10 13:02:44 +0300

    fix hw06: change assignment in task-06

--- src/ex6_js-array-methods/task-06.js
+++ src/ex6_js-array-methods/task-06.js
@@ -1,9 +1,12 @@
 function newReduce(array, callback, initialValue){
-    let previousValue = array[0];
-    let i = 1;
+    let previousValue, i;
     if (arguments.length > 2){
         previousValue = initialValue;
         i = 0;
+    }
+    else {
+        previousValue = array[0];
+        i = 1;
     }
     for (i; i < array.length; i++){
         previousValue = callback(previousValue, array[i], i, array);
